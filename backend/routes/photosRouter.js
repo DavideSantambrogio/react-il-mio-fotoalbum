7b8f5bb0167c35898756b875d78de5c5a481692d
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-06-26 17:28:30 +0200

    fix get photos without auth

--- backend/routes/photosRouter.js
+++ backend/routes/photosRouter.js
@@ -2,15 +2,17 @@ const express = require('express');
 const router = express.Router();
 const { body, param } = require('express-validator');
 const { createPhoto, getAllPhotos, getPhotoById, updatePhoto, deletePhoto, validatePhotoData, validatePhotoId } = require('../controllers/photoController');
+const authenticateToken = require('../middlewares/auth');
 
-router.post('/', validatePhotoData(), createPhoto);
+const validatePhotoDataMiddleware = validatePhotoData();
 
-router.get('/', getAllPhotos);
+const validatePhotoIdMiddleware = validatePhotoId();
 
-router.get('/:id', validatePhotoId(), getPhotoById);
-
-router.put('/:id', validatePhotoId(), validatePhotoData(), updatePhoto);
-
-router.delete('/:id', validatePhotoId(), deletePhoto);
+// Rotte per le foto
+router.post('/', authenticateToken, validatePhotoDataMiddleware, createPhoto); // Proteggi solo la creazione con autenticazione
+router.get('/', getAllPhotos); // Non proteggere la route GET
+router.get('/:id', validatePhotoIdMiddleware, getPhotoById);
+router.put('/:id', authenticateToken, validatePhotoIdMiddleware, validatePhotoDataMiddleware, updatePhoto); // Proteggi l'aggiornamento con autenticazione
+router.delete('/:id', authenticateToken, validatePhotoIdMiddleware, deletePhoto); // Proteggi l'eliminazione con autenticazione
 
 module.exports = router;
