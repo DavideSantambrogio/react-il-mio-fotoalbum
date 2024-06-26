1521bd71203edc6e25d8f24f86e4026a5a250512
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-06-26 17:40:24 +0200

    fix get photos without token per davvero stavolta

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
+  // Controllo per escludere la rotta GET /api/photos dall'autenticazione
+  if (req.method === 'GET' && req.originalUrl.startsWith('/api/photos')) {
+    return next(); // Passa alla gestione successiva senza autenticazione
+  }
+
   const token = req.header('Authorization')?.split(' ')[1];
 
   if (!token) return res.status(401).json({ message: 'Access denied, token missing!' });
